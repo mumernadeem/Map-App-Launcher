d6e42e2baca7422f3bc147f828906d9e72253177
up46629 <up46629@gmail.com>
2022-10-27 11:53:31 +0500

    Map App Launcher

---
+++ android/app/src/main/kotlin/com/example/google_map_app_launcher/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.google_map_app_launcher
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:maps_launcher/maps_launcher.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    super.key,
+  });
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FloatingActionButton(
+          onPressed: () {
+            MapsLauncher.launchCoordinates(37.4220041, -122.0862462);
+          },
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
